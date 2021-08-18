c9b9eed3685eed7095947bf86ad8f1b4b6cd6918
Guille <mr.guiller@gmail.com>
2021-08-18 20:03:13 +0200

    rudimentary controller

--- src/chip8.py
+++ src/chip8.py
@@ -1,18 +1,17 @@
+from typing import List
+
+
 class Chip8:
 
     def __init__(self) -> None:
         self.ram = Memory(4096)
-        self.display = Display()
+        self.display = Display(64, 32)
         self.pc = ProgramCounter()
         self.i = IndexRegister()
         self.stack = Stack()
         self.delay_timer = DelayTimer()
         self.sound_timer = SoundTimer()
-        self.registers = Registers()
-
-
-    def run(self):
-        pass
+        self.registers = Registers(16)
 
 
 class Memory:
@@ -27,8 +26,28 @@ class Memory:
 
 class Display:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, pixels_width: int, pixels_height: int) -> None:
+        self.pixels_width = pixels_width
+        self.pixels_height = pixels_height
+        self.pixels: List[List[bool]] = None
+
+        self.clear()
+
+
+    def clear(self) -> None:
+        self.pixels = [[False for i in range(self.pixels_width)] for i in range(self.pixels_height)]
+
+
+    def get_pixel_at(self, x: int, y: int) -> bool:
+        return self.pixels[y][x]
+
+
+    def turn_on_pixel_at(self, x: int, y: int) -> None:
+        self.pixels[y][x] = True
+
+
+    def turn_off_pixel_at(self, x: int, y: int) -> None:
+        self.pixels[y][x] = False
 
 
 class ProgramCounter:
@@ -65,8 +84,8 @@ class SoundTimer(Timer):
 
 class Registers:
 
-    def __init__(self) -> None:
-        self.registers = [0 for i in range(16)]
+    def __init__(self, count: int) -> None:
+        self.registers = [0 for i in range(count)]
 
 
     def __getitem__(self, i) -> int:
--- src/main.py
+++ src/main.py
@@ -1,9 +1,130 @@
-from chip8 import Chip8
+from typing import List
 
-def main():
-    chip8 = Chip8()
-    chip8.run()
+import pygame
+
+import chip8
+
+
+PIXEL_SIZE = 10
+PIXEL_ON_COLOR = (255, 255, 255)
+PIXEL_OFF_COLOR = (0, 0, 0)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_GEOMETRY = (SCREEN_WIDTH, SCREEN_HEIGHT)
+BACKGROUND_COLOR = (40, 40, 40)
+FONT_FAMILY = 'monospace'
+FONT_SIZE = 12
+
+
+class Engine:
+
+    def __init__(self) -> None:
+        pygame.init()
+        pygame.font.init()
+        self.font = pygame.font.SysFont(FONT_FAMILY, FONT_SIZE)
+
+        self.screen = pygame.display.set_mode(SCREEN_GEOMETRY, 0, 32)
+        pygame.display.set_caption('Mesh Viewer')
+
+        self.chip8 = chip8.Chip8()
+        self.init_drawables()
+
+
+    def init_drawables(self) -> None:
+        self.drawables: List[Drawable] = []
+
+        self.gamescreen = GameScreen(screen=self.screen, x=10, y=10, pixel_size=PIXEL_SIZE, chip8_display=self.chip8.display)
+        self.drawables.append(self.gamescreen)
+
+
+    def run(self) -> None:
+        clock = pygame.time.Clock()
+        self.running = True
+
+        while self.running:
+            self.manage_inputs()
+            self.update()
+            self.draw()
+            clock.tick(60)
+
+
+    def manage_inputs(self) -> None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+
+    def update(self) -> None:
+        pass
+
+
+    def draw(self) -> None:
+        self.screen.fill(BACKGROUND_COLOR)
+
+        for drawable in self.drawables:
+            drawable.draw()
+
+        pygame.display.update()
+
+
+class Drawable:
+
+    def __init__(self, screen: pygame.Surface, x: int, y: int, w: int = 0, h: int = 0) -> None:
+        self.screen = screen
+        self.x = x
+        self.y = y
+
+
+    @property
+    def width(self) -> int:
+        raise NotImplementedError()
+
+
+    @property
+    def height(self) -> int:
+        raise NotImplementedError()
+
+
+    def draw(self) -> None:
+        raise NotImplementedError()
+
+
+class GameScreen(Drawable):
+
+    def __init__(self, screen: pygame.Surface, x: int, y: int, pixel_size: int, chip8_display: chip8.Display) -> None:
+        super().__init__(screen, x, y)
+        self.pixel_size = pixel_size
+        self.chip8_display = chip8_display
+
+        self.w = self.chip8_display.pixels_width * self.pixel_size
+        self.h = self.chip8_display.pixels_height * self.pixel_size
+
+
+    @property
+    def width(self) -> int:
+        return self.chip8_display.pixels_width * self.pixel_size
+
+
+    @property
+    def height(self) -> int:
+        return self.chip8_display.pixels_height * self.pixel_size
+
+
+    def draw(self) -> None:
+        for y in range(self.chip8_display.pixels_height):
+            for x in range(self.chip8_display.pixels_width):
+                rect = pygame.Rect(
+                    self.x + x * self.pixel_size,
+                    self.y + y * self.pixel_size,
+                    self.pixel_size,
+                    self.pixel_size)
+
+                color = PIXEL_ON_COLOR if self.chip8_display.get_pixel_at(x, y) else PIXEL_OFF_COLOR
+
+                pygame.draw.rect(self.screen, color, rect)
 
 
 if __name__ == '__main__':
-    main()
+    engine = Engine()
+    engine.run()
