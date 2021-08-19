0c65951d2799fe61760cae3701ed0331bdbc463e
Guille <mr.guiller@gmail.com>
2021-08-20 00:13:00 +0200

    instruction class

--- src/chip8.py
+++ src/chip8.py
@@ -27,7 +27,31 @@ class Chip8:
 
 
     def step(self) -> None:
+        address = self.pc.value
+        instruction = Instruction(self.memory[address], self.memory[address+1])
+
         self.pc.increment()
+
+        print(instruction) # TODO
+
+
+class Instruction:
+
+    def __init__(self, msb: int, lsb: int) -> None:
+        self.k = Nibble(msb >> 4)
+        self.x = Nibble(msb & 0x0f)
+        self.y = Nibble(lsb >> 4)
+        self.n = Nibble(lsb & 0x0f)
+
+
+    @property
+    def hex(self) -> str:
+        return f'{self.k.hex}{self.x.hex}{self.y.hex}{self.n.hex}'
+
+
+    def __str__(self) -> str:
+        return self.hex
+
 
 
 class Memory:
@@ -183,3 +207,31 @@ class Registers:
     @property
     def flag_register(self) -> int:
         return self.registers[-1]
+
+
+class Nibble:
+
+    def __init__(self, value: int) -> None:
+        if type(value) != int:
+            raise ValueError(f'must be integer')
+        elif value < 0:
+            raise ValueError(f'must be non-negative integer')
+        elif value > 16:
+            raise ValueError(f'must be less than 16')
+
+        self._value = value
+
+
+    @property
+    def value(self) -> int:
+        return self._value
+
+
+    @property
+    def bin(self) -> str:
+        return str(bin(self._value)[2:]).zfill(4)
+
+
+    @property
+    def hex(self) -> str:
+        return ('0' + format(self._value, 'x'))[-1:].upper()
