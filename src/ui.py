f1541d9f7cbc525d8e03fa2b0c88bf1f680d887f
Guille <mr.guiller@gmail.com>
2021-08-19 21:35:37 +0200

    ui tweaks

--- src/ui.py
+++ src/ui.py
@@ -8,8 +8,9 @@ from constants import *
 
 class Drawable:
 
-    def __init__(self, screen: pygame.Surface, x: int = None, y: int = None, w: int = None, h: int = None) -> None:
+    def __init__(self, screen: pygame.Surface, font: pygame.font = None, x: int = None, y: int = None, w: int = None, h: int = None) -> None:
         self.screen = screen
+        self.font = font
         self.x = x
         self.y = y
         self.w = w
@@ -40,6 +41,21 @@ class Drawable:
         raise NotImplementedError()
 
 
+    def draw_text(self, lines: List[str], x_offset=0, y_offset=0, highlights: List[int] = None) -> None:
+        highlights = [] if highlights is None else highlights
+
+        for i, line in enumerate(lines):
+            color = HIGHLIGHT_COLOR if i in highlights else PRIMARY_COLOR
+
+            self.screen.blit(
+                self.font.render(line, False, color),
+                (MARGIN + self.x + x_offset, MARGIN + self.y + i * FONT_SIZE + y_offset))
+
+
+    def draw_frame(self) -> None:
+        pygame.draw.rect(self.screen, SECONDARY_COLOR, pygame.Rect(self.x, self.y, self.w, self.h), 1)
+
+
 class Panel(Drawable):
 
     def __init__(self, screen: pygame.Surface, chip8: chip8.Chip8) -> None:
@@ -49,66 +65,79 @@ class Panel(Drawable):
 
         self.drawables: List[Drawable] = []
 
-        games_creen = GameScreen(screen=screen, x=MARGIN, y=MARGIN, pixel_size=PIXEL_SIZE, chip8_display=chip8.display)
-        self.drawables.append(games_creen)
+        game_screen = GameScreen(screen=screen, x=MARGIN, y=MARGIN, pixel_size=PIXEL_SIZE, chip8_display=chip8.display)
+        self.drawables.append(game_screen)
 
         memory_view = MemoryView(
             screen=screen,
-            x=games_creen.right + MARGIN,
-            y=games_creen.top,
             font=self.font,
+            x=game_screen.right + MARGIN,
+            y=game_screen.top,
+            w=320,
+            h=532,
             chip8_memory=chip8.memory,
             chip8_pc=chip8.pc)
         self.drawables.append(memory_view)
 
         index_view = IndexView(
             screen=screen,
-            x=memory_view.right + MARGIN,
-            y=games_creen.top,
             font=self.font,
+            x=memory_view.right + MARGIN,
+            y=game_screen.top,
+            w=160,
+            h=52,
             chip8_index=chip8.index)
         self.drawables.append(index_view)
 
         delay_timer_view = TimerView(
             screen=screen,
+            font=self.font,
             x=memory_view.right + MARGIN,
             y=index_view.bottom + MARGIN,
-            font=self.font,
+            w=160,
+            h=52,
             name='Delay Timer',
             chip8_timer=chip8.delay_timer)
         self.drawables.append(delay_timer_view)
 
         sound_timer_view = TimerView(
             screen=screen,
+            font=self.font,
             x=memory_view.right + MARGIN,
             y=delay_timer_view.bottom + MARGIN,
-            font=self.font,
+            w=160,
+            h=52,
             name='Sound Timer',
             chip8_timer=chip8.delay_timer)
         self.drawables.append(sound_timer_view)
 
         registers_view = RegistersView(
             screen=screen,
+            font=self.font,
             x=memory_view.right + MARGIN,
             y=sound_timer_view.bottom + MARGIN,
-            font=self.font,
+            w=160,
+            h=164,
             chip8_registers=chip8.registers)
         self.drawables.append(registers_view)
 
         stack_view = StackView(
             screen=screen,
+            font=self.font,
             x=memory_view.right + MARGIN,
             y=registers_view.bottom + MARGIN,
-            font=self.font,
-            chip8_stack=chip8.stack,
-            length=memory_view.h - registers_view.bottom)
+            w=160,
+            h=memory_view.h - registers_view.bottom,
+            chip8_stack=chip8.stack)
         self.drawables.append(stack_view)
 
         help_text = HelpText(
             screen=screen,
+            font=self.font,
             x=MARGIN,
-            y=games_creen.bottom + MARGIN,
-            font=self.font)
+            y=game_screen.bottom + MARGIN,
+            w=440,
+            h=196)
         self.drawables.append(help_text)
 
 
@@ -120,7 +149,7 @@ class Panel(Drawable):
 class GameScreen(Drawable):
 
     def __init__(self, screen: pygame.Surface, x: int, y: int, pixel_size: int, chip8_display: chip8.Display) -> None:
-        super().__init__(screen, x, y, chip8_display.pixels_width * pixel_size, chip8_display.pixels_height * pixel_size)
+        super().__init__(screen=screen, x=x, y=y, w=chip8_display.pixels_width * pixel_size, h=chip8_display.pixels_height * pixel_size)
         self.pixel_size = pixel_size
         self.chip8_display = chip8_display
 
@@ -144,15 +173,24 @@ class GameScreen(Drawable):
             self.chip8_display.pixels_width * self.pixel_size,
             self.chip8_display.pixels_height * self.pixel_size)
 
-        pygame.draw.rect(self.screen, PRIMARY_COLOR, rect, 1)
+        self.draw_frame()
 
 
 class MemoryView(Drawable):
 
-    def __init__(self, screen: pygame.Surface, x: int, y: int, font: pygame.font, chip8_memory: chip8.Memory, chip8_pc: chip8.ProgramCounter) -> None:
-        super().__init__(screen, x, y, 320, 532)
+    def __init__(
+        self,
+        screen: pygame.Surface,
+        font: pygame.font,
+        x: int,
+        y: int,
+        w: int,
+        h: int,
+        chip8_memory: chip8.Memory,
+        chip8_pc: chip8.ProgramCounter) -> None:
 
-        self.font = font
+        super().__init__(screen, font, x, y, w, h)
+
         self.chip8_memory = chip8_memory
         self.chip8_pc = chip8_pc
 
@@ -174,23 +212,15 @@ class MemoryView(Drawable):
 
             marker = '→' if i == 0 else ' '
             lines.append(f'{marker}  {to_hex(address, 3)}  {to_hex(first_byte, 2)}{to_hex(second_byte, 2)}')
-        
-        for i, line in enumerate(lines):
-            color = HIGHLIGHT_COLOR if '→' in line else PRIMARY_COLOR
 
-            self.screen.blit(
-                self.font.render(line, False, color),
-                (MARGIN + self.x, MARGIN + self.y + i * FONT_SIZE))
-
-        pygame.draw.rect(self.screen, PRIMARY_COLOR, pygame.Rect(self.x, self.y, self.w, self.h), 1)
+        self.draw_text(lines, highlights=[16])
+        self.draw_frame()
 
 
 class IndexView(Drawable):
 
-    def __init__(self, screen: pygame.Surface, x: int, y: int, font: pygame.font, chip8_index: chip8.IndexRegister) -> None:
-        super().__init__(screen, x, y, 160, 52)
-
-        self.font = font
+    def __init__(self, screen: pygame.Surface, font: pygame.font, x: int, y: int, w: int, h: int, chip8_index: chip8.IndexRegister) -> None:
+        super().__init__(screen, font, x, y, w, h)
         self.chip8_index = chip8_index
 
 
@@ -199,13 +229,9 @@ class IndexView(Drawable):
             'Index register',
             f' {to_hex(self.chip8_index.address, 4)}',
         ]
-        
-        for i, line in enumerate(lines):
-            self.screen.blit(
-                self.font.render(line, False, PRIMARY_COLOR),
-                (MARGIN + self.x, MARGIN + self.y + i * FONT_SIZE))
 
-        pygame.draw.rect(self.screen, PRIMARY_COLOR, pygame.Rect(self.x, self.y, self.w, self.h), 1)
+        self.draw_text(lines)
+        self.draw_frame()
 
 
 class TimerView(Drawable):
@@ -213,15 +239,16 @@ class TimerView(Drawable):
     def __init__(
         self,
         screen: pygame.Surface,
+        font: pygame.font,
         x: int,
         y: int,
-        font: pygame.font,
+        w: int,
+        h: int,
         name: str,
         chip8_timer: chip8.DelayTimer) -> None:
 
-        super().__init__(screen, x, y, 160, 52)
+        super().__init__(screen, font, x, y, w, h)
 
-        self.font = font
         self.name = name
         self.chip8_timer = chip8_timer
 
@@ -231,104 +258,69 @@ class TimerView(Drawable):
             self.name,
             f' {to_hex(self.chip8_timer.value, 2)}',
         ]
-        
-        for i, line in enumerate(lines):
-            self.screen.blit(
-                self.font.render(line, False, PRIMARY_COLOR),
-                (MARGIN + self.x, MARGIN + self.y + i * FONT_SIZE))
 
-        pygame.draw.rect(self.screen, PRIMARY_COLOR, pygame.Rect(self.x, self.y, self.w, self.h), 1)
-
-
-class StackView(Drawable):
-
-    def __init__(self, screen: pygame.Surface, x: int, y: int, font: pygame.font, chip8_stack: chip8.Stack, length: int) -> None:
-        super().__init__(screen, x, y, 160, length)
-
-        self.font = font
-        self.chip8_stack = chip8_stack
-
-
-    def draw(self) -> None:
-        lines = []
-        lines.append('Stack')
-
-        for i in range(len(self.chip8_stack)):
-            lines.append(f' {to_hex(self.chip8_stack[i], 2)}')
-        
-        for i, line in enumerate(lines):
-            self.screen.blit(
-                self.font.render(line, False, PRIMARY_COLOR),
-                (MARGIN + self.x, MARGIN + self.y + i * FONT_SIZE))
-
-        pygame.draw.rect(self.screen, PRIMARY_COLOR, pygame.Rect(self.x, self.y, self.w, self.h), 1)
+        self.draw_text(lines)
+        self.draw_frame()
 
 
 class RegistersView(Drawable):
 
-    def __init__(self, screen: pygame.Surface, x: int, y: int, font: pygame.font, chip8_registers: chip8.Registers) -> None:
-        super().__init__(screen, x, y, 160, 164)
-
-        self.font = font
+    def __init__(self, screen: pygame.Surface, font: pygame.font, x: int, y: int, w: int, h: int, chip8_registers: chip8.Registers) -> None:
+        super().__init__(screen, font, x, y, w, h)
         self.chip8_registers = chip8_registers
 
 
     def draw(self) -> None:
-        lines = []
-        lines.append('Registers')
-
-        for i in range(len(self.chip8_registers) // 2):
-            j = i + len(self.chip8_registers) // 2
-
-            line = f' V{to_hex(i, 1)} {to_hex(self.chip8_registers[i], 2)}'
-            line += f'   V{to_hex(j, 1)} {to_hex(self.chip8_registers[j], 2)}'
-
-            lines.append(line)
+        register_count = len(self.chip8_registers)
+        lines = [f' V{to_hex(i, 1)} {to_hex(register, 2)}' for i, register in enumerate(self.chip8_registers)]
         
-        for i, line in enumerate(lines):
-            self.screen.blit(
-                self.font.render(line, False, PRIMARY_COLOR),
-                (MARGIN + self.x, MARGIN + self.y + i * FONT_SIZE))
+        self.draw_text(['Registers'])
+        self.draw_text(lines[:register_count//2], y_offset=FONT_SIZE)
+        self.draw_text(lines[register_count // 2:], x_offset=FONT_WIDTH*7, y_offset=FONT_SIZE)
 
-        pygame.draw.rect(self.screen, PRIMARY_COLOR, pygame.Rect(self.x, self.y, self.w, self.h), 1)
+        self.draw_frame()
+
+
+class StackView(Drawable):
+
+    def __init__(self, screen: pygame.Surface, font: pygame.font, x: int, y: int, w: int, h: int, chip8_stack: chip8.Stack) -> None:
+        super().__init__(screen, font, x, y, w, h)
+        self.chip8_stack = chip8_stack
+
+
+    def draw(self) -> None:
+        lines = [f' {to_hex(self.chip8_stack[i], 2)}' for i in range(len(self.chip8_stack))]
+
+        self.draw_text(['Stack'])
+        self.draw_text(lines[:8], y_offset=FONT_SIZE)
+        self.draw_text(lines[8:16], x_offset=FONT_WIDTH*4, y_offset=FONT_SIZE)
+        self.draw_text(lines[16:], x_offset=FONT_WIDTH*8, y_offset=FONT_SIZE)
+        self.draw_frame()
 
 
 class HelpText(Drawable):
 
-    def __init__(self, screen: pygame.Surface, x: int, y: int, font: pygame.font) -> None:
-        super().__init__(screen, x, y)
-        self.font = font
+    def __init__(self, screen: pygame.Surface, font: pygame.font, x: int, y: int, w: int, h: int) -> None:
+        super().__init__(screen, font, x, y, w, h)
+
         self.lines = [
             'Game controls',
             '',
-            '+---+---+---+---+        +---+---+---+---+',
-            '| 1 | 2 | 3 | C |   →    | 1 | 2 | 3 | 4 |',
-            '+---+---+---+---+        +---+---+---+---+',
-            '| 4 | 5 | 6 | D |   →    | Q | W | E | R |',
-            '+---+---+---+---+        +---+---+---+---+',
-            '| 7 | 8 | 9 | E |   →    | A | S | D | F |',
-            '+---+---+---+---+        +---+---+---+---+',
-            '| A | 0 | B | F |   →    | Z | X | C | V |',
-            '+---+---+---+---+        +---+---+---+---+',
+            '+---+---+---+---+       +---+---+---+---+',
+            '| 1 | 2 | 3 | C |   →   | 1 | 2 | 3 | 4 |',
+            '+---+---+---+---+       +---+---+---+---+',
+            '| 4 | 5 | 6 | D |   →   | Q | W | E | R |',
+            '+---+---+---+---+       +---+---+---+---+',
+            '| 7 | 8 | 9 | E |   →   | A | S | D | F |',
+            '+---+---+---+---+       +---+---+---+---+',
+            '| A | 0 | B | F |   →   | Z | X | C | V |',
+            '+---+---+---+---+       +---+---+---+---+',
         ]
 
 
     def draw(self) -> None:
-        for i, line in enumerate(self.lines):
-            self.screen.blit(
-                self.font.render(line, False, PRIMARY_COLOR),
-                (MARGIN + self.x, MARGIN + self.y + i * FONT_SIZE))
-
-        longest_line = max(self.lines, key=len)
-        longest_line_length = len(longest_line)
-
-        rect = pygame.Rect(
-            self.x,
-            self.y,
-            longest_line_length * FONT_WIDTH + 2 * MARGIN,
-            len(self.lines) * FONT_SIZE + 2 * MARGIN)
-
-        pygame.draw.rect(self.screen, PRIMARY_COLOR, rect, 1)
+        self.draw_text(self.lines)
+        self.draw_frame()
 
 
 def to_hex(num: int, min_digits: int) -> str:
